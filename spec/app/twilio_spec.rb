556c07e3f490c0b402d0ba9cd7f3f30749a6fb51
arnaud sellenet <arnodmental@gmail.com>
2013-02-21 16:24:31 +0100

    open and authorization specs

--- spec/app/twilio_spec.rb
+++ spec/app/twilio_spec.rb
@@ -3,23 +3,47 @@ require "spec_helper"
 describe 'Twilio' do
   include Rack::Test::Methods
   let(:number) { '12345' }
+  subject { post '/twilio/door', 'From' => number }
 
-  it "should respond to door" do
-    post '/twilio/door', { 'From' => number }
-    last_response.should be_ok
+  context "when konwn number" do
+    let(:is_authorized?) { false }
+    before do
+      Number.stub(:get).and_return 'number' => number, 'name' => 'joe', 'authorized' => is_authorized?
+    end
+
+
+    context "when not authorized" do
+
+      it { should be_ok }
+      it "should NOT open the door" do
+        Door.should_not_receive :open
+        subject
+      end
+    end
+
+    context "when authorized" do
+      let(:is_authorized?) { true }
+
+      it { should be_ok }
+      it "should open the door" do
+        Door.should_receive :open
+        subject
+      end
+    end
+  end
+
+
+  context "when unkonwn number" do
+
+    it { should be_ok }
+    it "should not open the door" do
+      Door.should_not_receive :open
+      subject
+    end
   end
 
   it "should respond on human" do
     get '/twilio/human', { 'From' => number, 'Digits' => '1' }
     last_response.should be_ok
-  end
-
-  context "when wrong number" do
-    let(:unknown_number) { '00000' }
-
-    it "should not open the door" do
-      Door.should_not_receive :open
-      post '/twilio/door', { 'From' => unknown_number }
-    end
   end
 end
