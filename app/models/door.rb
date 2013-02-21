91db12137a0fd69ab4a03558105386e10ed4e544
arnaud sellenet <arnodmental@gmail.com>
2013-02-21 16:58:30 +0100

    remove production-specific behaviour for door model

--- app/models/door.rb
+++ app/models/door.rb
@@ -2,10 +2,6 @@
 
 class Door
   def self.open
-    if production?
-      Net::HTTP.get_response(URI.parse (ENV['DOOR_URL']))
-    else
-      true
-    end
+    Net::HTTP.get_response(URI.parse (ENV['DOOR_URL']))
   end
 end
